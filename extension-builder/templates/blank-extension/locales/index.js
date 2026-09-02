e3cb06ef6a8ab11c8ef83c46490797d9c2e0523c
yi123dsckj <153899591+yi123dsckj@users.noreply.github.com>
2026-09-02 09:44:44 +0800

    Add blank extension translations

---
+++ extension-builder/templates/blank-extension/locales/index.js
@@ -0,0 +1,12 @@
+export default {
+    en: {
+        'ext.helloExample.name': 'My Extension',
+        'helloExample.sayHello': 'print [TEXT]',
+        'helloExample.helloText': 'hello text'
+    },
+    'zh-cn': {
+        'ext.helloExample.name': '我的扩展',
+        'helloExample.sayHello': '打印 [TEXT]',
+        'helloExample.helloText': '你好文字'
+    }
+};
