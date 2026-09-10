a151b345068eb1c0e677548a1d20c425b283a60b
yi123dsckj <yi123dsckj@users.noreply.github.com>
2026-09-10 13:13:06 +0800

    Restore verified v0.1.4 default robot IP

--- extension-builder/source/extension/index.js
+++ extension-builder/source/extension/index.js
@@ -71,7 +71,7 @@ class ExtensionBPX {
                     arguments: {
                         IP: {
                             type: ArgumentType.STRING,
-                            defaultValue: '10.21.20.1',
+                            defaultValue: '10.21.40.1',
                             inputParams: {symbol: '\"\"'}
                         }
                     }
@@ -181,3 +181,4 @@ class ExtensionBPX {
 }
 
 export default ExtensionBPX;
+
